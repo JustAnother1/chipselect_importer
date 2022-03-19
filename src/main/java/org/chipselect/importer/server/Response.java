f15b2cb5f4959e8745b0b6e1e82596ce4c002d75
Lars Pötter <kontakt@ing-poetter.de>
2022-03-19 03:26:50 +0100

    add support for longs.

--- src/main/java/org/chipselect/importer/server/Response.java
+++ src/main/java/org/chipselect/importer/server/Response.java
@@ -121,6 +121,63 @@ public class Response
         }
     }
 
+    public long getLong(String key)
+    {
+        return getLong(0, key);
+    }
+
+    public long getLong(int index, String key)
+    {
+        if(null == dataArr)
+        {
+            log.warn("No data array to read from");
+            return 0;
+        }
+        else
+        {
+            if(0 ==  dataArr.length())
+            {
+                log.warn("data array is empty");
+                return 0;
+            }
+            JSONObject obj = dataArr.getJSONObject(index);
+            if(false == obj.has(key))
+            {
+                log.warn("requested key not in data entry");
+                return 0;
+            }
+            if(true == obj.isNull(key))
+            {
+                log.warn("requested key({}) is null", key);
+                return 0;
+            }
+            try
+            {
+                return obj.getLong(key); // does not work with hex numbers like "0x400"
+            }
+            catch(JSONException e)
+            {
+                // might just be a hex number
+                try
+                {
+                    String hlp = obj.getString(key);
+                    long res = Long.decode(hlp);
+                    return res;
+                }
+                catch(JSONException e2)
+                {
+                    log.warn("JSON: Int convert Exception !(key: {} - {})", key, obj);
+                    return 0;
+                }
+                catch(NumberFormatException e1)
+                {
+                    log.warn("Int convert Exception !(key: {} - {})", key, obj);
+                    return 0;
+                }
+            }
+        }
+    }
+
     public String getString(String key)
     {
         return getString(0, key);
@@ -172,6 +229,31 @@ public class Response
                 return false;
             }
         }
+    }
+
+    public String dump(int index)
+    {
+        JSONObject obj = dataArr.getJSONObject(index);
+        if(null == obj)
+        {
+            return "";
+        }
+        return obj.toString();
+    }
+
+    public String dumpAllNames()
+    {
+        if(null == dataArr)
+        {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < numResults(); i++)
+        {
+            sb.append(getString(i, "name"));
+            sb.append(", ");
+        }
+        return sb.toString();
     }
 
 }
